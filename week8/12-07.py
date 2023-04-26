c1c79f0f5d081e9d160d03bcd279cecee3aa481d
gimmetype <gimmetype@gmail.com>
2023-04-26 16:09:33 +0900

    assignment week8

---
+++ week8/12-07.py
@@ -0,0 +1,12 @@
+class  Car :
+    def method(self):
+        print("슈퍼 클래스 ", end=' ')
+
+class Sedan(Car) :
+    def method(self):
+        print("서브 클래스 ", end=' ')
+
+myCar = Car()
+mySedan = Sedan()
+myCar.method()
+mySedan.method()
